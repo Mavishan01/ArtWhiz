23da1046f6d68cc0992ae3650a678ab0f524e23b
Mavishan01 <janithmavishan01@gmail.com>
2025-08-23 14:30:07 +0530

    server index.js

---
+++ server/index.js
@@ -0,0 +1,52 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb"}));
+app.use(express.urlencoded({ extended: true }));
+
+//error handler
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "There is an error";
+    return res.status(status).json({
+        success: false,
+        status,
+        message
+    });
+});
+
+app.get('/', async(req, res) => {
+    res.status(200).json({
+        message: "hello world",
+    });
+});
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Failed to connect");
+    console.error(err);
+    process.exit(1); // stop server if DB fails
+  }
+};
+
+// server start
+const startServer = async () => {
+    try {
+        connectDB();
+        app.listen(8080, () => console.log('server started on port 8080'));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
